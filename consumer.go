17d1cf3f93249a0dcbe5e3349299d5f1b2a51601
Nicolas Barbé <nicolas.barbe@magnolia-cms.com>
2015-09-04 11:38:43 +0200

    Add consumer

---
+++ consumer.go
@@ -0,0 +1,86 @@
+package kafka
+
+import ( 
+        "github.com/Shopify/sarama"
+        "log"
+)
+
+const (
+  bufferSize     = 256
+  initialOffset  = sarama.OffsetOldest // always start listening for the latest event. 
+)
+
+type Consumer struct {
+  consumer           sarama.Consumer
+  partitionConsumers []sarama.PartitionConsumer
+  messages           chan *sarama.ConsumerMessage
+}
+
+func NewConsumer(brokers []string, topic string) *Consumer {
+  config := sarama.NewConfig()
+  config.Consumer.Return.Errors = true
+
+  consumer, err := sarama.NewConsumer(brokers, config) 
+  if err != nil {
+    log.Fatalln(err)
+  }
+
+  partitions, err := consumer.Partitions(topic) 
+  if err != nil {
+    log.Println("Failed to get the list of partitions: %s", err)
+  }
+
+  partitionConsumers := make([]sarama.PartitionConsumer, len(partitions))
+  messages := make(chan *sarama.ConsumerMessage, bufferSize)
+
+  for _, partition := range partitions {
+
+    partitionConsumer, err := consumer.ConsumePartition(topic, partition, initialOffset)
+
+    if err != nil {
+      log.Fatalln("Failed to start consumer for partition %d: %s", partition, err)
+    }
+
+    go func(partitionConsumer sarama.PartitionConsumer) {
+      for message := range partitionConsumer.Messages() {
+        messages <- message
+      }
+    }(partitionConsumer)
+
+  }
+
+  return &Consumer{ 
+    consumer           : consumer, 
+    partitionConsumers : partitionConsumers,
+    messages           : messages,
+  }
+}
+
+// Consume messages and process them through the method pass in parameter
+func (this *Consumer) Consume(processMessage func(key, value []byte)) {
+go func() {
+    for message := range this.messages {
+      processMessage(message.Key, message.Value)
+    }
+  }()  
+}
+
+
+// Close stops processing messages and releases the corresponding resources
+func (this *Consumer) Close() {
+
+  log.Println("Done consuming messages")
+ 
+  for _, partitionConsumer := range this.partitionConsumers {
+    if err := partitionConsumer.Close(); err != nil {
+      log.Println("Failed to close partition consumer: ", err)
+    }
+  }
+
+  if err := this.consumer.Close(); err != nil {
+    log.Println("Failed to shutdown kafka consumer cleanly: %s", err)
+  }  
+
+  close(this.messages)
+
+}
